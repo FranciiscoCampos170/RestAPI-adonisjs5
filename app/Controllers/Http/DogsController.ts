35a01f4adb66307057936faf6191704df99e1557
Francisco Campos <franciiscocampos170@gmail.com>
2020-07-18 17:24:37 +0100

    feat: update and destroy method added

--- app/Controllers/Http/DogsController.ts
+++ app/Controllers/Http/DogsController.ts
@@ -12,4 +12,23 @@ export default class DogsController {
         const dog = await Dog.create(data)
         return dog
     }
+
+    public async show({ params }: HttpContextContract){
+        const data = await Dog.findOrFail(params.id)
+        return data
+    }
+
+    public async update({ request, params }: HttpContextContract){
+        const dog = await Dog.findOrFail(params.id)
+        const data = request.all()
+        dog.merge(data)
+        await dog.save()
+
+        return dog
+    }
+
+    public async destroy({params}){
+        const dog = await Dog.findOrFail(params.id)
+        await dog.delete()
+    }
 }
