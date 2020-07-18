abb956c46b51b70fa29cc57f35e62fb94865dcbf
Francisco Campos <franciiscocampos170@gmail.com>
2020-07-18 14:16:01 +0100

    migration seted

--- database/migrations/1595077470142_dogs.ts
+++ database/migrations/1595077470142_dogs.ts
@@ -5,7 +5,10 @@ export default class Dogs extends BaseSchema {
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+      table.increments('id').primary()
+      table.string('name')
+      table.string('breed')
+      table.text('description')
       table.timestamps(true)
     })
   }
